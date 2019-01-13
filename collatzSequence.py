e6acb01e45c74cc6f950f7a93114d8300f60e400
Falilat Shuaib <45600701+MsFalilat@users.noreply.github.com>
2019-01-13 12:02:55 -0500

    Update collatzSequence.py

--- collatzSequence.py
+++ collatzSequence.py
@@ -15,7 +15,8 @@ try:
     num = int(input()) #stores user input in variable 'num'
     
     while (collatz(num) != 1): #While the value of the function call on the user input does not equate to 1
-        num = collatz(num) #calls the collatz function on the user input and sets the variable storing the user input to the value evaluated by the function call
+        #calls collatz function on user input and sets variable storing user input to the value evaluated by the function call
+        num = collatz(num)
         print(num) #prints the current number that will be passed into the collatz function next
         
         if (collatz(num) == 1): #if the number evaluates to 1 after being passed to the collatz function, the while loop stops
